08af4f065334867adf2975e2537801c16a5b75a4
Andonys24 <andonihernandez1990@gmail.com>
2025-04-07 18:54:31 -0600

    * Correcion de errores:     - Solucion al error de sintaxis al eliminar el ptr de jugadores     - Identacion del codigo

--- Proyecto.cpp
+++ Proyecto.cpp
@@ -1,140 +1,124 @@
-#include <iostream>
-#include <iomanip>
-#include <stdlib.h>
-#include <limits>
 #include <ctime>
-
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <stdlib.h>
 
 using namespace std;
 
-//Estructura de jugador
-struct Jugador{  //Angel
- 
-	string Nombre;
-	int puntos=0;
+// Estructura de jugador
+struct Jugador { // Angel
 
+	string Nombre;
+	int puntos = 0;
 };
 
 // Prototipos Utilitarios
-void LimpiarConsola(); //Angel
-void generarTitulo(const string &); //Andony
-int validarEntrada(const string &); //Andony
-void pausarPrograma(const string &mensaje); //Andony
-
-
+void LimpiarConsola();						// Angel
+void generarTitulo(const string &);			// Andony
+int validarEntrada(const string &);			// Andony
+void pausarPrograma(const string &mensaje); // Andony
 
 // Prototipos del juego
-void dibujarDado(int cara); //Eduar
-int LanzarDado(); //Eduar 
-
+void dibujarDado(int cara); // Eduar
+int LanzarDado();			// Eduar
 
 // Funcion Principal
-int main(){
-
-	srand(time(NULL)); //Nahomy
-	int numjugadores,puntaje,dado;
-    char rsp, nombres[20];
-    
+int main() {
+	srand(time(NULL)); // Nahomy
+	int numjugadores, puntaje, dado;
+	char rsp, nombres[20];
 
 	generarTitulo("Juego de dados");
-	cout<<"\n\n";
-	
-    do {
-    
-    numjugadores = validarEntrada("Ingrese la cantidad de jugadores (2-5)");
+	cout << "\n\n";
 
-    if((numjugadores < 2 || numjugadores > 5)) cout<<"\nERROR. Ingrese un numero valido de jugadores.\n\n";
+	do {
+		numjugadores = validarEntrada("Ingrese la cantidad de jugadores (2-5)");
 
-    } while ((numjugadores < 2 || numjugadores > 5)); //Nahomy
+		if ((numjugadores < 2 || numjugadores > 5)) cout << "\nERROR. Ingrese un numero valido de jugadores.\n\n";
 
-   //Angel
-    Jugador *jugadores = new Jugador[numjugadores]; //crear el espacio para cada jugador
+	} while ((numjugadores < 2 || numjugadores > 5)); // Nahomy
 
-    //Nahomy
+	// Angel
+	Jugador *jugadores = new Jugador[numjugadores]; // crear el espacio para cada jugador
 
-    LimpiarConsola();
-        cin.clear();
-        cin.ignore();
-    for(int i=0; i<numjugadores; i++){
-       cout<<"\nIngrese el nombre de los jugadores: ";
-       cin.getline(nombres,20); 
-       jugadores[i].Nombre = nombres;
-    }
+	// Nahomy
 
-    puntaje = validarEntrada("\n\nIngrese el puntaje a alcanzar");
-    
-    do{
+	LimpiarConsola();
+	cin.clear();
+	cin.ignore();
+	for (int i = 0; i < numjugadores; i++) {
+		cout << "\nIngrese el nombre de los jugadores: ";
+		cin.getline(nombres, 20);
+		jugadores[i].Nombre = nombres;
+	}
 
-        for (int i = 0; i < numjugadores; i++)
-        {
-            while(true){
-                dado = LanzarDado();
-                generarTitulo("Turno de " + jugadores[i].Nombre);
+	puntaje = validarEntrada("\n\nIngrese el puntaje a alcanzar");
 
-                cout<<jugadores[i].Nombre << " ha sacado un " << dado <<"\n\n";
-                dibujarDado(dado);
+	do {
+		for (int i = 0; i < numjugadores; i++) {
+			while (true) {
+				dado = LanzarDado();
+				generarTitulo("Turno de " + jugadores[i].Nombre);
 
-                if(dado == 1){
+				cout << jugadores[i].Nombre << " ha sacado un " << dado << "\n\n";
+				dibujarDado(dado);
 
-                    jugadores[i].puntos = 0;
-                    cout<<jugadores[i].Nombre<<" a sacado 1, por ende pierde todos sus puntos.\n";
-                    pausarPrograma("Salir");
-                    break;
+				if (dado == 1) {
+					jugadores[i].puntos = 0;
+					cout << jugadores[i].Nombre << " a sacado 1, por ende pierde todos sus puntos.\n";
+					pausarPrograma("Salir");
+					break;
 
-                } //Nahomy
+				} // Nahomy
 
-                jugadores[i].puntos += dado;
-                
-                pausarPrograma("Salir");
+				jugadores[i].puntos += dado;
 
-                if(jugadores[i].puntos >= puntaje){
+				pausarPrograma("Salir");
 
-                    generarTitulo("GANADOR " + jugadores[i].Nombre);
+				if (jugadores[i].puntos >= puntaje) {
+					generarTitulo("GANADOR " + jugadores[i].Nombre);
 
-                    cout<<"FELICIDADES "<< jugadores[i].Nombre<<" HAS SIDO EL GANADOR!!!\n\n";
-                    delete jugadores[];
-                    pausarPrograma("Salir");
-                    return 1;
-                }
+					cout << "FELICIDADES " << jugadores[i].Nombre << " HAS SIDO EL GANADOR!!!\n\n";
+					delete[] jugadores;
+					pausarPrograma("Salir");
+					return 1;
+				}
 
-                cout<<"\n\n Desea volver a lanzar los dados? (Y/N)";
-                cin>>rsp;
+				cout << "\n\n Desea volver a lanzar los dados? (Y/N)";
+				cin >> rsp;
 
-                if((rsp == 'n') or (rsp == 'N')){
-                    break;
-                };
-            }
-            
-        }
-        
+				if ((rsp == 'n') or (rsp == 'N')) {
+					break;
+				};
+			}
+		}
 
+	} while (true);
 
-
-    }while(true);
-   
 	return 0;
 }
 
 // Funciones Utilitarias
 
-void LimpiarConsola() { //Angel
-#ifdef _WIN32 
+void LimpiarConsola() { // Angel
+#ifdef _WIN32
 	system("cls");
 #else
 	system("clear");
 #endif
 }
 
-void generarTitulo(const string &titulo) { //Andony
+void generarTitulo(const string &titulo) { // Andony
 	const string lineas(titulo.length() * 2, '*');
 
 	LimpiarConsola();
-	cout <<"\t\t"<< lineas << endl;
-	cout <<"\t\t"<< setw(titulo.length() + titulo.length() / 2) << titulo << endl;
-	cout <<"\t\t"<< lineas << endl << endl;
+	cout << "\t\t" << lineas << endl;
+	cout << "\t\t" << setw(titulo.length() + titulo.length() / 2) << titulo << endl;
+	cout << "\t\t" << lineas << endl << endl;
 }
 
-int validarEntrada(const string &mensaje) { //Andony
+int validarEntrada(const string &mensaje) { // Andony
 	int entrada = 0;
 
 	while (true) {
@@ -151,11 +135,10 @@ int validarEntrada(const string &mensaje) { //Andony
 	return entrada;
 }
 
-
-void pausarPrograma(const string &mensaje){ //Andony
-#ifdef _WIN32 
+void pausarPrograma(const string &mensaje) { // Andony
+#ifdef _WIN32
 	system("pause");
-    (void)mensaje;
+	(void)mensaje;
 #else
 	cin.ignore(numeric_limits<streamsize>::max(), '\n');
 	cout << endl << "Presione una tecla para " << mensaje << "...";
@@ -163,46 +146,46 @@ void pausarPrograma(const string &mensaje){ //Andony
 #endif
 }
 
-void dibujarDado(int cara) { //Eduar
-    cout << " ------- " << endl;
-    switch (cara) {
-        case 1:
-            cout << "|       |" << endl;
-            cout << "|   *   |" << endl;
-            cout << "|       |" << endl;
-            break;
-        case 2:
-            cout << "| *     |" << endl;
-            cout << "|       |" << endl;
-            cout << "|     * |" << endl;
-            break;
-        case 3:
-            cout << "| *     |" << endl;
-            cout << "|   *   |" << endl;
-            cout << "|     * |" << endl;
-            break;
-        case 4:
-            cout << "| *   * |" << endl;
-            cout << "|       |" << endl;
-            cout << "| *   * |" << endl;
-            break;
-        case 5:
-            cout << "| *   * |" << endl;
-            cout << "|   *   |" << endl;
-            cout << "| *   * |" << endl;
-            break;
-        case 6:
-            cout << "| *   * |" << endl;
-            cout << "| *   * |" << endl;
-            cout << "| *   * |" << endl;
-            break;
-    }
-    cout << " ------- " << endl;
+void dibujarDado(int cara) { // Eduar
+	cout << " ------- " << endl;
+	switch (cara) {
+	case 1:
+		cout << "|       |" << endl;
+		cout << "|   *   |" << endl;
+		cout << "|       |" << endl;
+		break;
+	case 2:
+		cout << "| *     |" << endl;
+		cout << "|       |" << endl;
+		cout << "|     * |" << endl;
+		break;
+	case 3:
+		cout << "| *     |" << endl;
+		cout << "|   *   |" << endl;
+		cout << "|     * |" << endl;
+		break;
+	case 4:
+		cout << "| *   * |" << endl;
+		cout << "|       |" << endl;
+		cout << "| *   * |" << endl;
+		break;
+	case 5:
+		cout << "| *   * |" << endl;
+		cout << "|   *   |" << endl;
+		cout << "| *   * |" << endl;
+		break;
+	case 6:
+		cout << "| *   * |" << endl;
+		cout << "| *   * |" << endl;
+		cout << "| *   * |" << endl;
+		break;
+	}
+	cout << " ------- " << endl;
 }
 
-int LanzarDado(){ //Eduar o Nohemy
+int LanzarDado() { // Eduar o Nohemy
 
-	int num=1+rand()%6;
+	int num = 1 + rand() % 6;
 
 	return num;
 }
